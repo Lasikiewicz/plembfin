09ef90d792593028fdb7b41751b0572ab014aaec
Lasikiewicz <jason@sam-tech.co.uk>
2026-06-18 16:28:16 +0100

    chore: clean up temporary scratch files

--- scratch/find_toggles.js
+++
@@ -1,9 +0,0 @@
-import fs from "node:fs";
-
-const html = fs.readFileSync("c:\\Github\\plembfin\\public\\index.html", "utf8");
-const lines = html.split("\n");
-lines.forEach((line, index) => {
-  if (line.includes("explorer-view-toggle")) {
-    console.log(`${index + 1}: ${line.trim()}`);
-  }
-});
