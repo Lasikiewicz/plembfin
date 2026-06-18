ad6f9f255eb886232a8b453f271e336f32d7962b
Lasikiewicz <jason@sam-tech.co.uk>
2026-06-18 16:28:16 +0100

    chore: clean up temporary scratch files

--- scratch/find_in_css.js
+++
@@ -1,9 +0,0 @@
-import fs from "node:fs";
-
-const css = fs.readFileSync("c:\\Github\\plembfin\\public\\styles.css", "utf8");
-const lines = css.split("\n");
-lines.forEach((line, index) => {
-  if (line.includes("explorer-heading")) {
-    console.log(`${index + 1}: ${line.trim()}`);
-  }
-});
