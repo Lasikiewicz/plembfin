ade6645a14be1af574a52994ce23087dc5a10f51
Lasikiewicz <jason@sam-tech.co.uk>
2026-09-14 08:51:55 +0100

    chore: update develop changelog

--- public/modules/help-content.js
+++ public/modules/help-content.js
@@ -1,5 +1,5 @@
-import { getWebhookToken } from "./auth.js?v=1.1.0.4.0";
-import { escapeHtml, escapeAttribute } from "./utils.js?v=1.1.0.4.0";
+import { getWebhookToken } from "./auth.js?v=1.1.0.4.1";
+import { escapeHtml, escapeAttribute } from "./utils.js?v=1.1.0.4.1";
 
 function snippet(code, language = "text") {
   const trimmed = String(code).trim();
