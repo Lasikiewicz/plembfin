78d6cdfc480470fb745caa2465027e34d20b36e2
Lasikiewicz <jason@sam-tech.co.uk>
2026-09-14 09:40:51 +0100

    chore: promote develop changelog to alpha

--- public/modules/help-content.js
+++ public/modules/help-content.js
@@ -1,5 +1,5 @@
-import { getWebhookToken } from "./auth.js?v=1.1.0.4.2";
-import { escapeHtml, escapeAttribute } from "./utils.js?v=1.1.0.4.2";
+import { getWebhookToken } from "./auth.js?v=1.1.0.5.0";
+import { escapeHtml, escapeAttribute } from "./utils.js?v=1.1.0.5.0";
 
 function snippet(code, language = "text") {
   const trimmed = String(code).trim();
